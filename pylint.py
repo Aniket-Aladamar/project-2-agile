cc91e8c0c357d79c97d800e7604e37fd3412b8a1
aniket <aniketmaladamar@gmail.com>
2023-11-02 20:24:06 +0530

    First commit

---
+++ pylint.py
@@ -0,0 +1,11 @@
+'''
+&quot;&quot;&quot;
+This is a simple program that demonstrates variable initialization and addition.
+&quot;&quot;&quot;
+'''
+# Initialize constant &#39;A&#39; with the value 1
+A = 1
+# Initialize constant &#39;B&#39; with the value 2
+B = 2
+# Print the sum of &#39;A&#39; and &#39;B&#39;
+print(A + B) # Output: 3
